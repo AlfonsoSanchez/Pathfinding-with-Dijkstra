7f5c3e6656bf96cd7bd00eb53970e399118760ff
AlfonsoSanchez <alfonsosp2@TR12.CAT>
2017-10-26 12:58:51 +0200

    A* GOOD Full Distance

--- Dev_class7.8_handout/Motor2D/j1Map.cpp
+++ Dev_class7.8_handout/Motor2D/j1Map.cpp
@@ -48,7 +48,7 @@ void j1Map::ResetPath()
 void j1Map::Path(int x, int y)
 {
 	path.Clear();
-
+	patfinding = false;
 	iPoint goal = WorldToMap(x, y);
 	int current = visited.find(goal);
 	if (current != -1)
@@ -66,7 +66,7 @@ void j1Map::Path(int x, int y)
 	{
 		path.Insert(goal, 0);
 	}
-//	path.PushBack(visited[current]);
+	//	path.PushBack(visited[current]);
 	// TODO 2: Follow the breadcrumps to goal back to the origin
 	// add each step into "path" dyn array (it will then draw automatically)
 }
@@ -75,101 +75,104 @@ void j1Map::PropagateAstar()
 {
 
 	iPoint curr;
-	
-	
-	if (frontier.Pop(curr))
+
+	if (patfinding == false)
 	{
 
-		int current = visited.find(curr);
-		iPoint last = breadcrumbs[current];
-		uint distance = sqrt(pow(last.x-path[0].x,2)+pow(last.y-path[0].y,2));
-		if (last.x != path[0].x && last.y != path[0].y)
-		{
-			if ((path[0].x != visited[current].x || path[0].y != visited[current].y) && patfinding == false)
-			{
-
-				iPoint neighbors[4];
-				neighbors[0].create(curr.x + 1, curr.y + 0);
-				neighbors[1].create(curr.x + 0, curr.y + 1);
-				neighbors[2].create(curr.x - 1, curr.y + 0);
-				neighbors[3].create(curr.x + 0, curr.y - 1);
-
-				for (uint i = 0; i < 4; ++i)
-				{
-					uint distance2 = sqrt(pow(neighbors[i].x - path[0].x, 2) + pow(neighbors[i].y - path[0].y, 2));
-					if (MovementCost(neighbors[i].x, neighbors[i].y) >= 0)
-					{
-
-						if (visited.find(neighbors[i]) == -1)
-						{
-							if (distance2 <= distance/* && (last.x + path[0].x >= neighbors[i].x + path[0].x && last.y  +path[0].y >= neighbors[i].y - path[0].y)|| (last.x + path[0].x <= neighbors[i].x + path[0].x && last.y + path[0].y <= neighbors[i].y + path[0].y)*/)
-							{
-								cost_so_far[neighbors[i].x][neighbors[i].y] = /*cost_so_far[last.x][last.y] + MovementCost(neighbors[i].x, neighbors[i].y) +*/ distance2;
-								frontier.Push(neighbors[i], cost_so_far[neighbors[i].x][neighbors[i].y]);
-								visited.add(neighbors[i]);
-								breadcrumbs.add(curr);
-							}
-
-						}
-					}
-				}
-			}
-			else
-			{
-				patfinding = true;
-			}
-		}
-		
-	}
-}
-
-void j1Map::PropagateDijkstra()
-{
-	
-	//data.layers.At();
-	// TODO 3: Taking BFS as a reference, implement the Dijkstra algorithm
-	// use the 2 dimensional array "cost_so_far" to track the accumulated costs
-	// on each cell (is already reset to 0 automatically)
-	iPoint curr;
-	
 		if (frontier.Pop(curr))
 		{
 
 			int current = visited.find(curr);
 			iPoint last = breadcrumbs[current];
-
-			if ((path[0].x != visited[current].x || path[0].y != visited[current].y) && patfinding == false)
+			int distance = sqrt(pow(last.x - path[0].x, 2) + pow(last.y - path[0].y, 2));
+			if (last.x != path[0].x || last.y != path[0].y)
 			{
-
-				iPoint neighbors[4];
-				neighbors[0].create(curr.x + 1, curr.y + 0);
-				neighbors[1].create(curr.x + 0, curr.y + 1);
-				neighbors[2].create(curr.x - 1, curr.y + 0);
-				neighbors[3].create(curr.x + 0, curr.y - 1);
-
-				for (uint i = 0; i < 4; ++i)
+				if ((path[0].x != visited[current].x || path[0].y != visited[current].y))
 				{
-					if (MovementCost(neighbors[i].x, neighbors[i].y) >= 0)
+
+					iPoint neighbors[4];
+					neighbors[0].create(curr.x + 1, curr.y + 0);
+					neighbors[1].create(curr.x + 0, curr.y + 1);
+					neighbors[2].create(curr.x - 1, curr.y + 0);
+					neighbors[3].create(curr.x + 0, curr.y - 1);
+
+					for (uint i = 0; i < 4; ++i)
 					{
-
-						if (visited.find(neighbors[i]) == -1)
+						int distance2 = sqrt(pow(neighbors[i].x - path[0].x, 2) + pow(neighbors[i].y - path[0].y, 2));
+						if (MovementCost(neighbors[i].x, neighbors[i].y) > 0)
 						{
-							cost_so_far[neighbors[i].x][neighbors[i].y] = cost_so_far[last.x][last.y] + MovementCost(neighbors[i].x, neighbors[i].y);
-							frontier.Push(neighbors[i], cost_so_far[neighbors[i].x][neighbors[i].y]);
-							visited.add(neighbors[i]);
-							breadcrumbs.add(curr);
 
+							if (visited.find(neighbors[i]) == -1)
+							{
+								if (distance2 <= distance)
+								{
+									cost_so_far[neighbors[i].x][neighbors[i].y] = distance2;
+									frontier.Push(neighbors[i], cost_so_far[neighbors[i].x][neighbors[i].y]);
+									visited.add(neighbors[i]);
+									breadcrumbs.add(curr);
+								}
+
+							}
 						}
 					}
 				}
+				else
+				{
+					patfinding = true;
+				}
 			}
-			else
-			{
-				patfinding = true;
-			}
+
 		}
 	}
-	
+}
+
+void j1Map::PropagateDijkstra()
+{
+
+	//data.layers.At();
+	// TODO 3: Taking BFS as a reference, implement the Dijkstra algorithm
+	// use the 2 dimensional array "cost_so_far" to track the accumulated costs
+	// on each cell (is already reset to 0 automatically)
+	iPoint curr;
+
+	if (frontier.Pop(curr))
+	{
+
+		int current = visited.find(curr);
+		iPoint last = breadcrumbs[current];
+
+		if ((path[0].x != visited[current].x || path[0].y != visited[current].y) && patfinding == false)
+		{
+
+			iPoint neighbors[4];
+			neighbors[0].create(curr.x + 1, curr.y + 0);
+			neighbors[1].create(curr.x + 0, curr.y + 1);
+			neighbors[2].create(curr.x - 1, curr.y + 0);
+			neighbors[3].create(curr.x + 0, curr.y - 1);
+
+			for (uint i = 0; i < 4; ++i)
+			{
+				if (MovementCost(neighbors[i].x, neighbors[i].y) >= 0)
+				{
+
+					if (visited.find(neighbors[i]) == -1)
+					{
+						cost_so_far[neighbors[i].x][neighbors[i].y] = cost_so_far[last.x][last.y] + MovementCost(neighbors[i].x, neighbors[i].y);
+						frontier.Push(neighbors[i], cost_so_far[neighbors[i].x][neighbors[i].y]);
+						visited.add(neighbors[i]);
+						breadcrumbs.add(curr);
+
+					}
+				}
+			}
+		}
+		else
+		{
+			patfinding = true;
+		}
+	}
+}
+
 
 
 int j1Map::MovementCost(int x, int y) const
@@ -193,9 +196,9 @@ void j1Map::PropagateBFS()
 {
 	// TODO 1: Record the direction to the previous node 
 	// with the new list "breadcrumps"
-	
+
 	iPoint curr;
-	
+
 	if (frontier.Pop(curr))
 	{
 		iPoint neighbors[4];
@@ -226,7 +229,7 @@ void j1Map::DrawPath()
 	// Draw visited
 	p2List_item<iPoint>* item = visited.start;
 
-	while(item)
+	while (item)
 	{
 		point = item->data;
 		TileSet* tileset = GetTilesetFromTileId(26);
@@ -261,24 +264,24 @@ void j1Map::DrawPath()
 
 void j1Map::Draw()
 {
-	if(map_loaded == false)
+	if (map_loaded == false)
 		return;
 
 	p2List_item<MapLayer*>* item = data.layers.start;
 
-	for(; item != NULL; item = item->next)
+	for (; item != NULL; item = item->next)
 	{
 		MapLayer* layer = item->data;
 
-		if(layer->properties.Get("Nodraw") != 0)
+		if (layer->properties.Get("Nodraw") != 0)
 			continue;
 
-		for(int y = 0; y < data.height; ++y)
+		for (int y = 0; y < data.height; ++y)
 		{
-			for(int x = 0; x < data.width; ++x)
+			for (int x = 0; x < data.width; ++x)
 			{
 				int tile_id = layer->Get(x, y);
-				if(tile_id > 0)
+				if (tile_id > 0)
 				{
 					TileSet* tileset = GetTilesetFromTileId(tile_id);
 
@@ -298,9 +301,9 @@ int Properties::Get(const char* value, int default_value) const
 {
 	p2List_item<Property*>* item = list.start;
 
-	while(item)
+	while (item)
 	{
-		if(item->data->name == value)
+		if (item->data->name == value)
 			return item->data->value;
 		item = item->next;
 	}
@@ -312,10 +315,10 @@ TileSet* j1Map::GetTilesetFromTileId(int id) const
 {
 	p2List_item<TileSet*>* item = data.tilesets.start;
 	TileSet* set = item->data;
-	
-	while(item)
+
+	while (item)
 	{
-		if(id < item->data->firstgid)
+		if (id < item->data->firstgid)
 		{
 			set = item->prev->data;
 			break;
@@ -331,12 +334,12 @@ iPoint j1Map::MapToWorld(int x, int y) const
 {
 	iPoint ret;
 
-	if(data.type == MAPTYPE_ORTHOGONAL)
+	if (data.type == MAPTYPE_ORTHOGONAL)
 	{
 		ret.x = x * data.tile_width;
 		ret.y = y * data.tile_height;
 	}
-	else if(data.type == MAPTYPE_ISOMETRIC)
+	else if (data.type == MAPTYPE_ISOMETRIC)
 	{
 		ret.x = (x - y) * (data.tile_width * 0.5f);
 		ret.y = (x + y) * (data.tile_height * 0.5f);
@@ -352,20 +355,20 @@ iPoint j1Map::MapToWorld(int x, int y) const
 
 iPoint j1Map::WorldToMap(int x, int y) const
 {
-	iPoint ret(0,0);
+	iPoint ret(0, 0);
 
-	if(data.type == MAPTYPE_ORTHOGONAL)
+	if (data.type == MAPTYPE_ORTHOGONAL)
 	{
 		ret.x = (x / data.tile_width);
 		ret.y = y / data.tile_height;
 	}
-	else if(data.type == MAPTYPE_ISOMETRIC)
+	else if (data.type == MAPTYPE_ISOMETRIC)
 	{
-		
+
 		float half_width = data.tile_width * 0.5f;
 		float half_height = data.tile_height * 0.5f;
-		ret.x = int( (x / half_width + y / half_height) / 2) - 1;
-		ret.y = int( (y / half_height - (x / half_width)) / 2);
+		ret.x = int((x / half_width + y / half_height) / 2) - 1;
+		ret.y = int((y / half_height - (x / half_width)) / 2);
 	}
 	else
 	{
@@ -396,7 +399,7 @@ bool j1Map::CleanUp()
 	p2List_item<TileSet*>* item;
 	item = data.tilesets.start;
 
-	while(item != NULL)
+	while (item != NULL)
 	{
 		RELEASE(item->data);
 		item = item->next;
@@ -407,7 +410,7 @@ bool j1Map::CleanUp()
 	p2List_item<MapLayer*>* item2;
 	item2 = data.layers.start;
 
-	while(item2 != NULL)
+	while (item2 != NULL)
 	{
 		RELEASE(item2->data);
 		item2 = item2->next;
@@ -428,30 +431,30 @@ bool j1Map::Load(const char* file_name)
 
 	pugi::xml_parse_result result = map_file.load_file(tmp.GetString());
 
-	if(result == NULL)
+	if (result == NULL)
 	{
 		LOG("Could not load map xml file %s. pugi error: %s", file_name, result.description());
 		ret = false;
 	}
 
 	// Load general info ----------------------------------------------
-	if(ret == true)
+	if (ret == true)
 	{
 		ret = LoadMap();
 	}
 
 	// Load all tilesets info ----------------------------------------------
 	pugi::xml_node tileset;
-	for(tileset = map_file.child("map").child("tileset"); tileset && ret; tileset = tileset.next_sibling("tileset"))
+	for (tileset = map_file.child("map").child("tileset"); tileset && ret; tileset = tileset.next_sibling("tileset"))
 	{
 		TileSet* set = new TileSet();
 
-		if(ret == true)
+		if (ret == true)
 		{
 			ret = LoadTilesetDetails(tileset, set);
 		}
 
-		if(ret == true)
+		if (ret == true)
 		{
 			ret = LoadTilesetImage(tileset, set);
 		}
@@ -461,24 +464,24 @@ bool j1Map::Load(const char* file_name)
 
 	// Load layer info ----------------------------------------------
 	pugi::xml_node layer;
-	for(layer = map_file.child("map").child("layer"); layer && ret; layer = layer.next_sibling("layer"))
+	for (layer = map_file.child("map").child("layer"); layer && ret; layer = layer.next_sibling("layer"))
 	{
 		MapLayer* lay = new MapLayer();
 
 		ret = LoadLayer(layer, lay);
 
-		if(ret == true)
+		if (ret == true)
 			data.layers.add(lay);
 	}
 
-	if(ret == true)
+	if (ret == true)
 	{
 		LOG("Successfully parsed map XML file: %s", file_name);
 		LOG("width: %d height: %d", data.width, data.height);
 		LOG("tile_width: %d tile_height: %d", data.tile_width, data.tile_height);
 
 		p2List_item<TileSet*>* item = data.tilesets.start;
-		while(item != NULL)
+		while (item != NULL)
 		{
 			TileSet* s = item->data;
 			LOG("Tileset ----");
@@ -489,7 +492,7 @@ bool j1Map::Load(const char* file_name)
 		}
 
 		p2List_item<MapLayer*>* item_layer = data.layers.start;
-		while(item_layer != NULL)
+		while (item_layer != NULL)
 		{
 			MapLayer* l = item_layer->data;
 			LOG("Layer ----");
@@ -510,7 +513,7 @@ bool j1Map::LoadMap()
 	bool ret = true;
 	pugi::xml_node map = map_file.child("map");
 
-	if(map == NULL)
+	if (map == NULL)
 	{
 		LOG("Error parsing map xml file: Cannot find 'map' tag.");
 		ret = false;
@@ -528,7 +531,7 @@ bool j1Map::LoadMap()
 		data.background_color.b = 0;
 		data.background_color.a = 0;
 
-		if(bg_color.Length() > 0)
+		if (bg_color.Length() > 0)
 		{
 			p2SString red, green, blue;
 			bg_color.SubString(1, 2, red);
@@ -538,26 +541,26 @@ bool j1Map::LoadMap()
 			int v = 0;
 
 			sscanf_s(red.GetString(), "%x", &v);
-			if(v >= 0 && v <= 255) data.background_color.r = v;
+			if (v >= 0 && v <= 255) data.background_color.r = v;
 
 			sscanf_s(green.GetString(), "%x", &v);
-			if(v >= 0 && v <= 255) data.background_color.g = v;
+			if (v >= 0 && v <= 255) data.background_color.g = v;
 
 			sscanf_s(blue.GetString(), "%x", &v);
-			if(v >= 0 && v <= 255) data.background_color.b = v;
+			if (v >= 0 && v <= 255) data.background_color.b = v;
 		}
 
 		p2SString orientation(map.attribute("orientation").as_string());
 
-		if(orientation == "orthogonal")
+		if (orientation == "orthogonal")
 		{
 			data.type = MAPTYPE_ORTHOGONAL;
 		}
-		else if(orientation == "isometric")
+		else if (orientation == "isometric")
 		{
 			data.type = MAPTYPE_ISOMETRIC;
 		}
-		else if(orientation == "staggered")
+		else if (orientation == "staggered")
 		{
 			data.type = MAPTYPE_STAGGERED;
 		}
@@ -581,7 +584,7 @@ bool j1Map::LoadTilesetDetails(pugi::xml_node& tileset_node, TileSet* set)
 	set->spacing = tileset_node.attribute("spacing").as_int();
 	pugi::xml_node offset = tileset_node.child("tileoffset");
 
-	if(offset != NULL)
+	if (offset != NULL)
 	{
 		set->offset_x = offset.attribute("x").as_int();
 		set->offset_y = offset.attribute("y").as_int();
@@ -600,7 +603,7 @@ bool j1Map::LoadTilesetImage(pugi::xml_node& tileset_node, TileSet* set)
 	bool ret = true;
 	pugi::xml_node image = tileset_node.child("image");
 
-	if(image == NULL)
+	if (image == NULL)
 	{
 		LOG("Error parsing tileset xml file: Cannot find 'image' tag.");
 		ret = false;
@@ -612,14 +615,14 @@ bool j1Map::LoadTilesetImage(pugi::xml_node& tileset_node, TileSet* set)
 		SDL_QueryTexture(set->texture, NULL, NULL, &w, &h);
 		set->tex_width = image.attribute("width").as_int();
 
-		if(set->tex_width <= 0)
+		if (set->tex_width <= 0)
 		{
 			set->tex_width = w;
 		}
 
 		set->tex_height = image.attribute("height").as_int();
 
-		if(set->tex_height <= 0)
+		if (set->tex_height <= 0)
 		{
 			set->tex_height = h;
 		}
@@ -641,7 +644,7 @@ bool j1Map::LoadLayer(pugi::xml_node& node, MapLayer* layer)
 	LoadProperties(node, layer->properties);
 	pugi::xml_node layer_data = node.child("data");
 
-	if(layer_data == NULL)
+	if (layer_data == NULL)
 	{
 		LOG("Error parsing map xml file: Cannot find 'layer/data' tag.");
 		ret = false;
@@ -653,7 +656,7 @@ bool j1Map::LoadLayer(pugi::xml_node& node, MapLayer* layer)
 		memset(layer->data, 0, layer->width*layer->height);
 
 		int i = 0;
-		for(pugi::xml_node tile = layer_data.child("tile"); tile; tile = tile.next_sibling("tile"))
+		for (pugi::xml_node tile = layer_data.child("tile"); tile; tile = tile.next_sibling("tile"))
 		{
 			layer->data[i++] = tile.attribute("gid").as_int(0);
 		}
@@ -669,11 +672,11 @@ bool j1Map::LoadProperties(pugi::xml_node& node, Properties& properties)
 
 	pugi::xml_node data = node.child("properties");
 
-	if(data != NULL)
+	if (data != NULL)
 	{
 		pugi::xml_node prop;
 
-		for(prop = data.child("property"); prop; prop = prop.next_sibling("property"))
+		for (prop = data.child("property"); prop; prop = prop.next_sibling("property"))
 		{
 			Properties::Property* p = new Properties::Property();
 
